9a14e60b20d115f588a660d42a6c0246ef0ff3cf
FREDVUNI <fredvuni809@gmail.com>
2023-07-21 13:40:24 +0300

    chore: spell enqueue correctly

--- themes/vuni/functions.php
+++ themes/vuni/functions.php
@@ -1,12 +1,12 @@
 <?php
 // action hook
 // dependencies are the scripts that other scripts depend on so they can't load before them (the array is the dependency)
-add_action('wp_enqueque_scripts', function () {
-    wp_enqueque_style("main-styles", get_stylesheet_directory_uri() . "/assets/css/main.css");
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style("main-styles", get_stylesheet_directory_uri() . "/assets/css/main.css");
 
-    wp_enqueque_script("jquery", get_stylesheet_directory_uri() . "/assets/js/jquery.min.js");
-    wp_enqueque_script("scrollex", get_stylesheet_directory_uri() . "/assets/js/jquery.min.js", array('jquery'));
-    wp_enqueque_script("skel", get_stylesheet_directory_uri() . "/assets/js/skel.min.js", array('jquery'));
-    wp_enqueque_script("util", get_stylesheet_directory_uri() . "/assets/js/util.js", array('jquery'));
-    wp_enqueque_script("main-js-script", get_stylesheet_directory_uri() . "/assets/js/main.js", array('jquery', 'scrollex', 'skil', 'util'));
+    wp_enqueue_script("jquery", get_stylesheet_directory_uri() . "/assets/js/jquery.min.js");
+    wp_enqueue_script("scrollex", get_stylesheet_directory_uri() . "/assets/js/jquery.min.js", array('jquery'));
+    wp_enqueue_script("skel", get_stylesheet_directory_uri() . "/assets/js/skel.min.js", array('jquery'));
+    wp_enqueue_script("util", get_stylesheet_directory_uri() . "/assets/js/util.js", array('jquery'));
+    wp_enqueue_script("main-js-script", get_stylesheet_directory_uri() . "/assets/js/main.js", array('jquery', 'scrollex', 'skil', 'util'));
 });
